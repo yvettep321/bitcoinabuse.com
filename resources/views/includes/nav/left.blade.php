a0f5c47329122a2c47adb5766c7509cbe66c9358
Alan Reed <alan@reedtechsol.com>
2020-07-07 11:02:38 -0400

    update telescope link

--- resources/views/includes/nav/left.blade.php
+++ resources/views/includes/nav/left.blade.php
@@ -3,5 +3,5 @@
 	<li class="nav-item mx-1"><a href="/reports" class="nav-link"><b>View Reports</b></a></li>
 	<li class="nav-item mx-1"><a href="/faq" class="nav-link"><b>FAQ</b></a></li>
     <li class="nav-item mx-1"><a href="/api-docs" class="nav-link"><b>API Docs</b></a></li>
-    <li class="nav-item mx-1"><a href="/api-docs" class="nav-link"><b>View on Github <i class="fab fa-github"></i></b></a></li>
+    <li class="nav-item mx-1"><a href="https://github.com/aarreedd/bitcoinabuse.com" class="nav-link" target=_blank><b>View on Github <i class="fab fa-github"></i></b></a></li>
 </ul>
