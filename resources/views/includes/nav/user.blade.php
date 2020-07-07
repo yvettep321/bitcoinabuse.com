8b6a004f25db3a1f668e7cd95acfb8396fb2ab86
Alan Reed <alan@reedtechsol.com>
2020-07-07 10:36:16 -0400

    add telescope link to admin nav

--- resources/views/includes/nav/user.blade.php
+++ resources/views/includes/nav/user.blade.php
@@ -17,29 +17,6 @@
 			@include('includes.nav.left')
 
 
-			{{--
-			<a @click="showNotifications" class="notification-pill mx-auto mb-3 mb-md-0 mr-md-0 ml-md-auto">
-			<svg class="mr-2" width="18px" height="20px" viewBox="0 0 18 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-				<defs>
-					<linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="linearGradient-1">
-						<stop stop-color="#86A0A6" offset="0%"></stop>
-						<stop stop-color="#596A79" offset="100%"></stop>
-					</linearGradient>
-				</defs>
-				<g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-					<g id="header" transform="translate(-926.000000, -29.000000)" fill-rule="nonzero" fill="url(#linearGradient-1)">
-						<g id="Group-3">
-							<path d="M929,37 C929,34.3773361 930.682712,32.1476907 933.027397,31.3318031 C933.009377,31.2238826 933,31.1130364 933,31 C933,29.8954305 933.895431,29 935,29 C936.104569,29 937,29.8954305 937,31 C937,31.1130364 936.990623,31.2238826 936.972603,31.3318031 C939.317288,32.1476907 941,34.3773361 941,37 L941,43 L944,45 L944,46 L926,46 L926,45 L929,43 L929,37 Z M937,47 C937,48.1045695 936.104569,49 935,49 C933.895431,49 933,48.1045695 933,47 L937,47 L937,47 Z"
-								  id="Combined-Shape"></path>
-						</g>
-					</g>
-				</g>
-			</svg>
-			@{{notificationsCount}}
-			</a>
-			--}}
-
-
 			<ul class="navbar-nav ml-auto">
 
 				<li class="mr-2 my-3 my-lg-0">
@@ -73,6 +50,10 @@
 							<!-- Kiosk -->
 							<a class="dropdown-item" href="/spark/kiosk">
 								<i class="fab text-left fa-btn fa-fort-awesome"></i> {{__('Kiosk')}}
+							</a>
+
+							<a class="dropdown-item" href="/telescope">
+								<i class="fas text-left fa-btn fa-binoculars"></i> {{__('Telescope')}}
 							</a>
 
 							<div class="dropdown-divider"></div>
@@ -109,7 +90,7 @@
 					</div>
 				</li>
 			</ul>
-				
+
 
 		</div>
 	</nav>
