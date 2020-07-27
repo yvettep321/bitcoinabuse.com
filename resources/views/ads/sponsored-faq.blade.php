39b2f4d4625fcfdcab19c518e1910841ff881818
Alan Reed <alan@reedtechsol.com>
2020-07-26 23:27:04 -0400

    add sponsored #1 #3

--- resources/views/ads/sponsored-faq.blade.php
+++ resources/views/ads/sponsored-faq.blade.php
@@ -1,10 +1,14 @@
 {{-- Sponsored FAQ text goes here --}}
-{{-- All links must include rel="sponsored" --}}
-
-{{-- <h4 class="mt-5"> What is ABC? <span class="badge badge-secondary">Sponsored</span> </h4>
+<h4 class="mt-5 font-weight-bold" style="color:#bd10e0;">
+	How can Blockchain Intelligence Group help?
+	<span class="badge badge-secondary">Sponsored</span>
+</h4>
 <p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+	Visually track, trace and monitor Bitcoin transactions with Blockchain Intelligence Group's forensics tool
+	<a href="https://qlue.io/qlue/?utm_source=bitcoinabuse&utm_medium=faq" target="_blank" rel="sponsored">QLUE</a>.
+	Get a crypto equivalent of a credit score for the addresses you deal with and follow the money behind these transactions.
+	Blockchain Intelligence Group offers a
+	<a href="http://https://bigcs.com/bitrankqlue/?utm_source=bitcoinabuse&utm_medium=faq" target="_blank" rel="sponsored">compliance suite</a>
+	that allow for sophisticated cryptocurrency forensics and real-time risk mitigation.
+	For more info, contact us at <a href="mailto:info@blockchaingroup.io" rel="sponsored">info@blockchaingroup.io</a>.
 </p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-</p> --}}
